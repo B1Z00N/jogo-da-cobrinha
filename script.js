cb46e96b97351733723ea7c5f445314498a923d6
B1Z00N <mrcrusher61@gmail.com>
2021-07-07 17:43:19 -0300

    adiciona os movimentos a cobra

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ snake[0] = {
         x: 8 * box,
         y: 8 * box
 }
+let direction = "right";
+
 
 function criarBG() {
 context.fillStyle = "purple";
@@ -20,7 +22,29 @@ context.fillRect(snake [i].x, snake[i].y, box, box);
 }
 
 }
-
-
+function iniciarjogo(){
 criarBG();
 criarcobra();
+
+let snakeX = [0].x;
+let snakeY = [0].y;
+
+
+if(direction =="right") snakeX += box;
+if(direction =="left" ) snakeY -= box;
+if(direction =="up" ) snakeY -= box;
+if(direction =="down") snakeY += box;
+
+snake.pop();
+
+let newhead = {
+X: snakeX,
+y:snakeY
+}
+snake.unshift(newhead);
+
+
+
+}
+
+let jogo = setInterval(iniciarjogo, 100);
